d841b5d9fe2c849f1852d8160a2114537ac3b302
Deepak <100xdeepak@gmail.com>
2026-07-31 18:25:17 +0530

    Stage 1: root and health endpoints

--- src/app.js
+++ src/app.js
@@ -2,4 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.get('/describe' , (req,res)=>{
+    return res.json({
+        name : "Describe API",
+        version : "1.0",
+        endpoints : "/health"
+    })
+})
+
+app.get('/health' , (req,res)=>{
+    return res.json({
+        status : "ok"
+    })
+})
 export default app;
